a6f66afedb7d36d7053739dc6c973da7ffbc4d9c
dmitriy.p <dmitriy.p@filancy.com>
2018-10-29 09:20:41 +0200

    Init

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+function initEvents() {
+    /*Actions on 'DOM ready' event*/
+
+    /*Actions on 'Window load' event*/
+    $(window).on("load", function() {
+        
+    });
+};
+
+/*Start all functions and actions*/
+initEvents();
---
+++ js/index.js
@@ -0,0 +1,1 @@
+function initEvents(){$(function(){}),$(window).on("load",function(){})}initEvents();
